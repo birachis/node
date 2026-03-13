9c1317e623fc681421a01704971a52d777f02c41
birachis <birumoa@gmail.com>
2026-03-13 08:24:19 +0300

    Exercises

--- module1/typescript/src/app.ts
+++ module1/typescript/src/app.ts
@@ -1,5 +1,18 @@
-function greeting(name: string): string {
-  return `Hello, ${name}!`;
+import { fetchData } from "./data_fetcher/fetcher.js";
+
+async function main() {
+  const result = await fetchData({
+    url: "https://jsonplaceholder.typicode.com/posts/1",
+    method: "GET",
+    timeout: 1000,
+    retries: 3,
+  });
+
+  if (result.success) {
+    console.log("Data:", result.data);
+  } else {
+    console.error("Error:", result.error);
+  }
 }
 
-console.log(greeting("Moa Biru"));
+main();
